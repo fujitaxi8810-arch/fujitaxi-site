e7d53e01cca1391eac3e00075590a72c7171687b
fujitaxi8810-arch <fujitaxi8810@gmail.com>
2026-08-01 11:03:29 +0900

    seo: link Google Business Profile listings (honsha + haramachi) in structured data sameAs

--- src/lib/structuredData.ts
+++ src/lib/structuredData.ts
@@ -66,5 +66,7 @@ export const localBusinessSchema = {
     'https://www.instagram.com/fuji_taxi442543/',
     'https://www.tiktok.com/@fujitaxi8810',
     'https://line.me/R/ti/p/@580lzpne',
+    'https://www.google.com/maps/place/%E3%88%B2%E5%AF%8C%E5%A3%AB%E3%82%BF%E3%82%AF%E3%82%B7%E3%83%BC/data=!4m7!3m6!1s0x6020953600cf6d2b:0x54fa84d84b926304!8m2!3d37.5638552!4d140.9911752!16s%2Fg%2F11f0_b2t0_!19sChIJK23PADaVIGARBGOSS9iE-lQ',
+    'https://www.google.com/maps/place/%28%E6%9C%89%29%E5%AF%8C%E5%A3%AB%E3%82%BF%E3%82%AF%E3%82%B7%E3%83%BC%E5%8E%9F%E7%94%BA%E5%BE%85%E6%A9%9F%E6%89%80+%E5%8D%97%E7%9B%B8%E9%A6%AC%E5%B8%82/data=!4m7!3m6!1s0x6020954451ace2b9:0xcb8e83fef1b7b3e8!8m2!3d37.6272827!4d140.9850973!16s%2Fg%2F11rqwl4vbq!19sChIJueKsUUSVIGAR6LO38f6Djss',
   ],
 };
